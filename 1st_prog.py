3cff4d4040b2f836683141dcdfe4d89184083ef6
Veja Shetty <vejashetty@gmail.com>
2024-07-29 12:44:04 +0530

    Create 1st_prog.py

---
+++ 1st_prog.py
@@ -0,0 +1,23 @@
+import csv
+
+with open('enjoysport.csv', 'r') as csvfile:
+    data = list(csv.reader(csvfile))
+
+print(data)
+
+print("\nThe total number of training instances are:", len(data))
+
+num_attributes = len(data[0]) - 1
+hypothesis = ['0'] * num_attributes
+print("\nThe initial hypothesis is:", hypothesis)
+
+for i, instance in enumerate(data):
+    if instance[num_attributes] == 'yes':
+        for j in range(num_attributes):
+            if hypothesis[j] == '0' or hypothesis[j] == instance[j]:
+                hypothesis[j] = instance[j]
+            else:
+                hypothesis[j] = '?'
+    print(f"\nThe hypothesis after training instance {i + 1} is:", hypothesis)
+
+print("\n\nThe Maximally specific hypothesis is:", hypothesis)
